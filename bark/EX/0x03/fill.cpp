fc8b05789dd50bb69232058a6c5920956ad1a331
Kim-TaeUk <xodnr0130@naver.com>
2024-06-02 02:45:35 +0900

    EX - 배열 전체 특정 값으로 초기화(fill)

---
+++ bark/EX/0x03/fill.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    int a[21];
+    int b[21][21];
+
+    fill(a, a + 21, 0);
+
+    for (int i = 0; i < 21; i++) {
+        fill(b[i], b[i] + 21, 0);
+    }
+}
